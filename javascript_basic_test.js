cf645b980266c26e19eb69b9852727781d0948fd
FrancisOlasupo <francisolasupo90@gmail.com>
2024-07-29 23:23:05 +0200

    initial commit: all questions answered!

---
+++ javascript_basic_test.js
@@ -0,0 +1,32 @@
+// a. Create an array named “myStudent”
+const myStudent = [
+  'Aguguom',
+  'Joseph',
+  'Michael',
+  'Dominion',
+  'Emmanuel',
+  'Francis',
+  'Chimaobi',
+  'Nwabueze',
+  'Alarape',
+  'Tinny',
+];
+
+// b. Create a for loop which runs to console each item in the array
+for (let i = 0; i < myStudent.length; i++) {
+  console.log(myStudent[i]);
+}
+
+// c. Create an object with property (name: string, age: number, married: Boolean, location: undefined)
+const person = {
+  name: 'Augustin David',
+  age: 60,
+  married: true,
+  location: 'Lagos',
+};
+
+// d. Console each property in the object (using dot notation)
+console.log(person.name);
+console.log(person.age);
+console.log(person.married);
+console.log(person.location);
